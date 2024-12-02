c381e0e5d0a6f6cdc3d8786022d287018c3a0a16
Wendy Dherin <wendydherin@gmail.com>
2024-12-02 08:35:48 -0800

    Add open-simplx library and two new sketches.

--- menger-sponge.js
+++ menger-sponge.js
@@ -1,55 +1,65 @@
-let a = 0;
-let b = 0;
-let c = 0;
-const sponge = [];
-
-// function Box() {
-
-//   function generate() {
-//     for (let x = -1, x < 2; x++) {
-//       for (let y = -1, y < 2; y++) {
-//         for (let z = -1, z < 2; z++) {
-//           sponge.push(box())
-//         }
-
-//       }
-
-//     }
-//   }
-// }
-
-function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
-  // An array of Box objects
-  // Star with one
-  var b = new Box(0, 0, 0, 200);
-  sponge.push(b);
+class Box {
+  constructor(x, y, z, r_) {
+    this.position = createVector(x, y, z)
+    this.r = r_
+  }
+  show() {
+    push()
+    translate(this.position.x, this.position.y, this.position.z)
+    fill(255)
+    box(this.r)
+    pop()
+  }
+  generate() {
+    const boxes = []
+    for (let x = -1; x < 2; x++) {
+      for (let y = -1; y < 2; y++) {
+        for (let z = -1; z < 2; z++) {
+          const sum = abs(x) + abs(y) + abs(z)
+          const newR = this.r / 3
+          if (sum > 1) {
+            boxes.push(
+              new Box(
+                this.position.x + x * newR,
+                this.position.y + y * newR,
+                this.position.z + z * newR,
+                newR
+              )
+            )
+          }
+        }
+      }
+    }
+    return boxes
+  }
 }
 
-function mousePressed() {
-  // Generate the next set of boxes
-  var next = [];
-  for (var i = 0; i < sponge.length; i++) {
-    var b = sponge[i];
-    var newBoxes = b.generate();
-    next = next.concat(newBoxes);
-  }
-  sponge = next;
+let a = 0
+let sponge = []
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL)
+  normalMaterial()
+  sponge.push(new Box(0, 0, 0, 200))
 }
 
 function draw() {
-  background(50);
-  stroke(255);
-  noFill();
-
-  rotateX(a);
-  rotateY(b);
-  rotateZ(c);
-  // box(height / 2);
-  for (var i = 0; i < sponge.length; i++) {
-    sponge[i].show();
+  background(0)
+  lights()
+  rotateX(a)
+  rotateY(a * 0.4)
+  rotateZ(a * 0.2)
+  for (let i = 0; i < sponge.length; i++) {
+    sponge[i].show()
   }
-  a += 0.01;
-  b += 0.01;
-  c += 0.01;
+  a += 0.01
+}
+
+function mousePressed() {
+  let next = []
+  for (const b of sponge) {
+    const newBoxes = b.generate()
+    next = next.concat(newBoxes)
+  }
+  sponge = next
 }
